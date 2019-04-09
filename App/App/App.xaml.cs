ddd5b6362533cec885a4f886c7a0d42276739030
LIXINXIN <lixinxin@travelsky.com>
2019-04-09 19:45:33 +0800

    uwp第一章

---
+++ App/App/App.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace App
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MyApp : Application
+    {
+        private TransitionCollection transitions;
+        public MyApp()
+        {
+            this.InitializeComponent();
+            this.Suspending +=this.OnSuspending;
+        }
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            //MainPage page = new MainPage();
+            //base.OnLaunched(args);
+            //Window.Current.Content = page;
+            Frame rootFrame = Window.Current.Content as Frame;
+            if(rootFrame == null)
+            {
+                rootFrame = new Frame();
+                rootFrame.CacheSize = 1;
+
+                if(args.PreviousExecutionState == ApplicationExecutionState.Terminated||args.PreviousExecutionState == ApplicationExecutionState.ClosedByUser)
+                {
+                    UserData.LoadData();
+                }
+            }
+            Window.Current.Content = rootFrame;
+            //root.Navigate(typeof(MainPage));
+            if(rootFrame.Content == null)
+            {
+                if(rootFrame.ContentTransitions != null)
+                {
+                    this.transitions = new TransitionCollection();
+                    foreach(var c in rootFrame.ContentTransitions)
+                    {
+                        this.transitions.Add(c);
+                    }
+                }
+                rootFrame.ContentTransitions = null;
+                rootFrame.Navigated += this.RootFrame_FirstNavigated;
+                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
+                {
+                    throw new Exception("failed");
+                }
+            }
+            Window.Current.Activate();
+        }
+        private void RootFrame_FirstNavigated(object sender, NavigationEventArgs e)
+        {
+            var rootFrame = sender as Frame;
+            rootFrame.ContentTransitions = this.transitions ?? new TransitionCollection() { new NavigationThemeTransition() };
+            rootFrame.Navigated -= RootFrame_FirstNavigated;
+        }
+        private void OnSuspending(object sender,SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            UserData.SaveData();
+            deferral.Complete();
+        }
+    }
+}
---
+++ App/App/UserData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace App
+{
+    class UserData : INotifyPropertyChanged
+    {
+        #region
+        private static UserData _userData = null;
+        private int _count = default(int);
+        #endregion
+        static UserData()
+        {
+            _userData = new UserData();
+        }
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                if(value != _count)
+                {
+                    _count = value;
+                    OnPropertyChangde();
+                }
+            }
+        }
+        public static UserData CurrentInstance
+        {
+            get
+            {
+                return _userData;
+            }
+        }
+        public static void LoadData()
+        {
+            object settingVal = null;
+            if (ApplicationData.Current.LocalSettings.Values.TryGetValue("num",out settingVal))
+            {
+                CurrentInstance.Count = Convert.ToInt32(settingVal);
+            }
+            else
+            {
+                CurrentInstance.Count = 0;
+            }
+        }
+        public static void SaveData()
+        {
+            ApplicationData.Current.LocalSettings.Values["num"] = CurrentInstance.Count;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChangde([CallerMemberName] string propName = "")
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
