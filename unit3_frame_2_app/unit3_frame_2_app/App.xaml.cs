d40392425b8d387e699e6e18f631ee8c61733eef
LIXINXIN <lixinxin@travelsky.com>
2019-04-12 19:39:57 +0800

    Frame学习，导航事件的触发顺序。当frame对象跳转时，触发OnNavigating,之后触发OnNavigated.当页面跳转时，Page会触发OnNavigatingFrom,之后OnNavigatedFrom,页面显示触发OnNavigatedTo

---
+++ unit3_frame_2_app/unit3_frame_2_app/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace unit3_frame_2_app
+{
+    /// <summary>
+    /// 提供特定于应用程序的行为，以补充默认的应用程序类。
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// 初始化单一实例应用程序对象。这是执行的创作代码的第一行，
+        /// 已执行，逻辑上等同于 main() 或 WinMain()。
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            //this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// 在应用程序由最终用户正常启动时进行调用。
+        /// 将在启动应用程序以打开特定文件等情况下使用。
+        /// </summary>
+        /// <param name="e">有关启动请求和过程的详细信息。</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            MainPage main = new MainPage();
+            Window.Current.Content = main;
+            Window.Current.Activate();
+        }
+    }
+}
---
+++ unit3_frame_2_app/unit3_frame_2_app/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace unit3_frame_2_app
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            string item = e.ClickedItem as string;
+            if (!string.IsNullOrEmpty(item))
+            {
+                switch (item)
+                {
+                    case "Page 1":
+                        fame.Navigate(typeof(Page1));
+                        break;
+                    case "Page 2":
+                        fame.Navigate(typeof(Page2));
+                        break;
+                    case "Page 3":
+                        fame.Navigate(typeof(Page3));
+                        break;
+                }
+            }
+        }
+
+        private void OnFrameNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            string msg = "Frame的Navigating事件发生，SourcePageType = {0}";
+            System.Diagnostics.Debug.WriteLine(msg, e.SourcePageType);
+        }
+
+        private void OnFrameNavigated(object sender, NavigationEventArgs e)
+        {
+            string msg = "Frame的Navigated事件发生，SourcePageType = {0}";
+            System.Diagnostics.Debug.WriteLine(msg, e.SourcePageType);
+        }
+    }
+}
