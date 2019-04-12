bf7f8ffab4ba8b246c47785d76591202c6108d20
LIXINXIN <lixinxin@travelsky.com>
2019-04-12 20:21:47 +0800

    Frame 页面之间传递信息

---
+++ unit3_frame_3_app/unit3_frame_3_app/DisplayPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace unit3_frame_3_app
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class DisplayPage : Page
+    {
+        public DisplayPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Dictionary<string, string> data = (Dictionary<string, string>)e.Parameter;
+            tbID.Text = data["id"];
+            tbName.Text = data["name"];
+            tbAge.Text = data["age"];
+        }
+    }
+}
---
+++ unit3_frame_3_app/unit3_frame_3_app/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace unit3_frame_3_app
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            if(txtID.Text == ""||txtName.Text ==""||txtAge.Text == "")
+            {
+                return;
+            }
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            data.Add("id", txtID.Text);
+            data.Add("name", txtName.Text);
+            data.Add("age", txtAge.Text);
+            this.Frame.Navigate(typeof(DisplayPage), data);
+        }
+    }
+}
