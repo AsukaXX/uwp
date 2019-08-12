c99e80c2c03059876cad54b14172f693940ba617
Asuka <asukalxx@outlook.com>
2019-08-12 20:39:51 +0800

    下拉框，纯文本，数据绑定，下拉框关闭事件触发

---
+++ Examples_4_15/Examples_4_15/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Examples_4_15
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            listPickerFlyout.ItemsSource = new List<string> { "诺基亚", "三星", "索尼" };
+        }
+
+        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+            menuFlyoutButton.Content = (sender as MenuFlyoutItem).Text;
+        }
+
+        private async void DatePickerFlyout_DatePicked(DatePickerFlyout sender, DatePickedEventArgs args)
+        {
+            await new MessageDialog(args.NewDate.ToString()).ShowAsync();
+        }
+
+        private async void TimePickerFlyout_TimePicked(TimePickerFlyout sender, TimePickedEventArgs args)
+        {
+            await new MessageDialog(args.NewTime.ToString()).ShowAsync();
+        }
+
+        private async void PickerFlyout_Confirmed(PickerFlyout sender, PickerConfirmedEventArgs args)
+        {
+            await new MessageDialog("你点了确定").ShowAsync();
+        }
+
+        private async void ListPickerFlyout_ItemsPicked(ListPickerFlyout sender, ItemsPickedEventArgs args)
+        {
+            if (sender.SelectedItem != null)
+            {
+                await new MessageDialog("你选择的是：" + sender.SelectedItem.ToString()).ShowAsync();
+            }
+        }
+
+        private void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            FrameworkElement element = sender as FrameworkElement;
+            if (element != null)
+            {
+                FlyoutBase.ShowAttachedFlyout(element);
+            }
+        }
+    }
+}
---
+++ Examples_4_16/Examples_4_16/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Examples_4_16
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            List<Man> datas = new List<Man>
+            {
+                new Man{Name = "张三",Age=20},
+                new Man{Name="李四",Age=34},
+                new Man{Name="王五" ,Age=43},
+                new Man{Name="刘六",Age=33},
+                new Man{Name="王七",Age=44}
+            };
+            comboBox2.ItemsSource = datas;
+        }
+
+        private void ComboBox2_DropDownClosed(object sender, object e)
+        {
+            if (comboBox2.SelectedItem != null)
+            {
+                Man man = comboBox2.SelectedItem as Man;
+                Info.Text = "name: " + man.Name + " age: " + man.Age;
+            }
+        }
+    }
+
+    public class Man
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
