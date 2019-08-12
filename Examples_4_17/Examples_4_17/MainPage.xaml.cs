5121d00e5ae944b0be14e6e3627d7d1e80ce626c
Asuka <asukalxx@outlook.com>
2019-08-12 21:05:33 +0800

    命令栏，菜单栏：菜单图标，文字菜单

---
+++ Examples_4_17/Examples_4_17/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Examples_4_17
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CommandBar_Opened(object sender, object e)
+        {
+            info.Text = "菜单栏打开了";
+        }
+
+        private void CommandBar_Closed(object sender, object e)
+        {
+            info.Text = "菜单栏关闭了";
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            info.Text = "单击了菜单栏：" + (sender as AppBarButton).Label;
+        }
+
+    }
+}
